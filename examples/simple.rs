74c097b7e6c970f8a7a5461c288455d2d21c2551
Anton Whalley <anton@venshare.com>
2020-07-13 00:40:48 +0100

    'static struct

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,26 +2,32 @@ use tide_handlebars::prelude::*;
 use handlebars::Handlebars;
 use std::collections::BTreeMap;
 
+struct HandlebarsEngine {
+    registry: Handlebars<'static>
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+
     tide::log::start();
 
-    let mut handlebars = Handlebars::new();
-    handlebars
-        .register_template_file("simple.html", "./templates/simple.html")
-        .unwrap();
-    let mut app = tide::with_state(handlebars);
-    app.listen("127.0.0.1:8080").await?;
+    let mut engine = HandlebarsEngine {
+        registry: Handlebars::new()
+    };
 
+    engine.registry.register_template_file("simple.html", "./examples/templates/simple.html").unwrap();
+
+    let mut app = tide::with_state(engine);
     app.at("/:name")
-        .get(|req: tide::Request<Handlebars>| async move {
-            let hb = req.state();
+        .get(|req: tide::Request<HandlebarsEngine>| async move {
+            let hb = &req.state().registry;
             let name: String = req.param("name")?;
             let mut data0 = BTreeMap::new();
             data0.insert("name".to_string(), name);
-            hb.render_response("hello.html", &data0)
+            Ok(hb.render_response("simple.html", &data0)?)
         });
 
+    app.listen("127.0.0.1:8080").await?;
 
     Ok(())
 }
