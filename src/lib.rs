ec303909bc0f9055a8ffa2d62ccd54578b4edfcb
Anton Whalley <anton@venshare.com>
2020-07-14 01:12:25 +0100

    added support for overriding extension discovery

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,28 @@ pub trait TideHandlebarsExt {
     fn render_body<T>(&self, template_name: &str, context: &T) -> Result<Body>
     where
         T: Serialize;
-
+    /// `render_body_ext` returns a fully-rendered [`tide::Body`] with mime
+    /// type set based on the extension using the
+    /// logic at [`tide::http::Mime::from_extension`]. This will
+    /// return an `Err` variant if the render was unsuccessful.
+    ///
+    /// ```rust
+    /// use handlebars::Handlebars;
+    /// use tide_handlebars::prelude::*;
+    /// use std::collections::BTreeMap;
+    /// let mut handlebars = Handlebars::new();
+    ///     handlebars
+    ///     .register_template_file("simple.hbs", "./tests/templates/simple.hbs")
+    ///     .unwrap();
+    ///
+    /// let mut data0 = BTreeMap::new();
+    /// data0.insert("title".to_string(), "hello tide!".to_string());
+    /// let mut body = handlebars.render_body_ext("simple.hbs", &data0, "html").unwrap();
+    /// assert_eq!(body.mime(), &tide::http::mime::HTML);
+    ///```
+    fn render_body_ext<T>(&self, template_name: &str, context: &T, extension: &str) -> Result<Body>
+    where
+        T: Serialize;
     /// `render_response` returns a tide Response with a body rendered
     /// with [`render_body`](TideHandlebarsExt::render_body). This will
     /// return an `Err` variant if the render was unsuccessful.
@@ -55,23 +76,54 @@ pub trait TideHandlebarsExt {
     fn render_response<T>(&self, template_name: &str, context: &T) -> Result
     where
         T: Serialize;
+    /// `render_response_ext` returns a tide Response with a body rendered
+    /// with [`render_body`](TideHandlebarsExt::render_body). This will
+    /// return an `Err` variant if the render was unsuccessful.
+    ///
+    /// ```rust
+    /// use handlebars::Handlebars;
+    /// use tide_handlebars::prelude::*;
+    /// use std::collections::BTreeMap;
+    /// let mut handlebars = Handlebars::new();
+    /// handlebars
+    ///     .register_template_file("simple.hbs", "./tests/templates/simple.hbs")
+    ///     .unwrap();
+    /// let mut data0 = BTreeMap::new();
+    /// data0.insert("title".to_string(), "hello tide!".to_string());
+    /// let mut response = handlebars.render_response_ext("simple.hbs", &data0, "html").unwrap();
+    /// assert_eq!(response.content_type(), Some(tide::http::mime::HTML));
+    ///```
+    fn render_response_ext<T>(&self, template_name: &str, context: &T, extension: &str) -> Result
+    where
+        T: Serialize;
 }
 
 impl TideHandlebarsExt for Handlebars<'_> {
-    fn render_body<T>(&self, template_name: &str, context: &T) -> Result<Body>
+    fn render_body_ext<T>(&self, template_name: &str, context: &T, extension: &str) -> Result<Body>
     where
         T: Serialize,
     {
         let string = self.render(template_name, context)?;
         let mut body = Body::from_string(string);
+        if let Some(mime) = Mime::from_extension(extension) {
+            body.set_mime(mime);
+        }
+        Ok(body)
+    }
 
+    fn render_body<T>(&self, template_name: &str, context: &T) -> Result<Body>
+    where
+        T: Serialize,
+    {
+        let string = self.render(template_name, context)?;
+        
         let path = PathBuf::from(template_name);
+        let mut body = Body::from_string(string);
         if let Some(extension) = path.extension() {
             if let Some(mime) = Mime::from_extension(extension.to_string_lossy()) {
-                body.set_mime(mime)
+                body.set_mime(mime);
             }
         }
-
         Ok(body)
     }
 
@@ -81,6 +133,14 @@ impl TideHandlebarsExt for Handlebars<'_> {
     {
         let mut response = Response::new(200);
         response.set_body(self.render_body(template_name, context)?);
+        Ok(response)
+    }
+    fn render_response_ext<T>(&self, template_name: &str, context: &T, extension: &str) -> Result
+    where
+        T: Serialize,
+    {
+        let mut response = Response::new(200);
+        response.set_body(self.render_body_ext(template_name, context, extension)?);
         Ok(response)
     }
 }
@@ -146,7 +206,36 @@ mod tests {
 
         assert_eq!(body.mime(), &tide::http::mime::PLAIN);
     }
+    #[test]
+    fn body_with_extension() {
+        let mut handlebars = Handlebars::new();
+        handlebars
+            .register_templates_directory(".hbs", "./tests/templates")
+            .unwrap();
 
+        let mut data0 = BTreeMap::new();
+        data0.insert("title".to_string(), "hello tide!".to_string());
+        let body = handlebars.render_body_ext("simple", &data0, "html").unwrap();
+
+        assert_eq!(body.mime(), &tide::http::mime::HTML);
+    }
+    #[async_std::test]
+    async fn response_with_extension() {
+        let mut handlebars = Handlebars::new();
+        handlebars
+        .register_templates_directory(".hbs", "./tests/templates")
+            .unwrap();
+        let mut data0 = BTreeMap::new();
+        data0.insert("title".to_string(), "hello tide!".to_string());
+
+        let mut response = handlebars.render_response_ext("simple", &data0, "html").unwrap();
+
+        assert_eq!(response.content_type(), Some(tide::http::mime::HTML));
+
+        let http_response: &mut tide::http::Response = response.as_mut();
+        let body_string = http_response.body_string().await.unwrap();
+        assert_eq!(body_string, "<h1>hello tide!</h1>\n");
+    }
     // Templates are validate on load in handlebars -- need to work into the component
     // #[test]
     // fn bad_template() {
